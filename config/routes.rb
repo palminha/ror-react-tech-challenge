17c3738f6b33ef71f39bd537b5ed207195b6fde4
Carlos Palminha <carlos.palminha@gmail.com>
2021-07-10 17:01:26 +0100

    About is now the default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'about#index'
   get 'about', to: 'about#index'
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
