74bd3648616916f2889f88a17afbc5db3d6ed9a3
Carlos Palminha <carlos.palminha@gmail.com>
2021-07-10 17:49:17 +0100

    Add a new component where the NavBar will reside

---
+++ app/javascript/components/nav_bar/NavBar.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { useState, FunctionComponent } from 'react';
+
+function NavBar () { 
+    return (
+      <h3>The NavBar will be here</h3>
+    );
+  }
+  
+  export default NavBar;
+  
---
+++ app/javascript/packs/application-components.tsx
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+import NavBar from '../components/nav_bar/NavBar';
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+  NavBar,
+});
